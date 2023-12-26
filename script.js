961c939000d82c9cd1e26a91b0229a4da20d521e
Pedro1301h <pedro1301@gmail.com>
2023-12-26 17:37:57 -0300

    Subindo configurações de tela

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 let btn = document.querySelector('#btn');
 let saidas = document.querySelector('#saidas')
 
-btn.addEventListener('click',function criaVetor(){    //Parâmtros(Quantos jogos, quantos números)
+btn.addEventListener('click',function sorteador(){    //Parâmtros(Quantos jogos, quantos números)
     let vet = [];    //Cria o vetor que receberá os valores
     let sorteado;
-    for(let i = 1; i <= 10;i++){  
+    for(let i = 1; i <= 30;i++){  
         if(i > 9){  
-            saidas.innerHTML+=`<p><br>Jogo ${i}</p>`
+            saidas.innerHTML+=`<p>Jogo ${i}</p>`
         }else{
-            saidas.innerHTML+=`<p><br>Jogo 0${i}</p>`
+            saidas.innerHTML+=`<p>Jogo 0${i}</p>`
         }
-        for(let c = 0;c < 15; c++){
+        for(let c = 0;c < 6; c++){
             sorteado = Math.floor(Math.random() * 61);
             if((vet.indexOf(sorteado) == -1) && (sorteado != 0)){
                 vet.push(sorteado);
                 if(sorteado > 9){
-                    saidas.innerHTML+=`<span class="num">${sorteado}</span><br>`
+                    saidas.innerHTML+=`<span class="num">${sorteado}</span>`
                 }else{
-                    saidas.innerHTML+=`<span class="num">0${sorteado}</span><br>`
+                    saidas.innerHTML+=`<span class="num">0${sorteado}</span>`
                 }
             }else{
                 c--;
@@ -28,7 +28,7 @@ btn.addEventListener('click',function criaVetor(){    //Parâmtros(Quantos jogos
         console.log('J'+i+': '+vet);
         vet = [];
     }      
-    btn.removeEventListener('click',criaVetor)
+    btn.removeEventListener('click',sorteador)
 })
 
 
