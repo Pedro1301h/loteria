7fc291e851d9167fb19cea0518354dffe700bf58
Pedro1301h <pedro1301@gmail.com>
2023-12-27 17:14:50 -0300

    Organizando a lógica

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 
 let btn = document.querySelector('#btn');
-let saidas = document.querySelector('#saidas')
+let saidas = document.querySelector('#saidas');
+let sel = document.querySelectorAll('#select-jogo');
+let qntJogos = document.querySelector('#quant-jogos');
 
-btn.addEventListener('click',function sorteador(){    //Parâmtros(Quantos jogos, quantos números)
-    let vet = [];    //Cria o vetor que receberá os valores
-    let sorteado;
-    for(let i = 1; i <= 30;i++){  
+let qntNums;
+let mult;
+let vet = []; 
+let sorteado;
+
+btn.addEventListener('click',function sorteador(){   
+    selecionarJogo();
+    sorteio();
+});
+
+function reset(){
+    saidas.innerHTML='';
+    vet=[];
+}
+
+function selecionarJogo(){
+    if(sel[0].value == 'mega'){
+        reset();
+        qntNums = 6;
+        mult = 61;
+        console.log(vet);
+    }
+    if(sel[0].value == 'loto'){
+        reset();
+        qntNums = 15;
+        mult = 26;
+        console.log(vet);
+    }
+}
+
+function sorteio(){
+    for(let i = 1; i <= qntJogos.value;i++){  
         if(i > 9){  
-            saidas.innerHTML+=`<p>Jogo ${i}</p>`
+            saidas.innerHTML+=`<p>Jogo ${i}</p>`;
         }else{
-            saidas.innerHTML+=`<p>Jogo 0${i}</p>`
+            saidas.innerHTML+=`<p>Jogo 0${i}</p>`;
         }
-        for(let c = 0;c < 6; c++){
-            sorteado = Math.floor(Math.random() * 61);
+        for(let c = 0;c < qntNums; c++){
+            sorteado = Math.floor(Math.random() * mult);
             if((vet.indexOf(sorteado) == -1) && (sorteado != 0)){
                 vet.push(sorteado);
                 if(sorteado > 9){
-                    saidas.innerHTML+=`<span class="num">${sorteado}</span>`
+                    saidas.innerHTML+=`<span class="num">${sorteado}</span>`;
                 }else{
-                    saidas.innerHTML+=`<span class="num">0${sorteado}</span>`
+                    saidas.innerHTML+=`<span class="num">0${sorteado}</span>`;
                 }
             }else{
                 c--;
             }
         }
-
-        console.log('J'+i+': '+vet);
         vet = [];
     }      
-    btn.removeEventListener('click',sorteador)
-})
-
-
-
-
+}
